4254b76ed3da7e599c2e88544581441490ca8255
Bram Pieters <bram95pieters@gmail.com>
2018-10-02 17:42:03 +0200

    les5

---
+++ ADOps/ADOps/BinarySearchTree.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOps
+{
+    interface IBinarySearchTree
+    {
+        void Insert(int x);
+        void Remove(int x);
+        void RemoveMin();
+        int FindMin();
+        string InOrder();
+        string ToString();
+    }
+
+    class BinarySearchTree : IBinarySearchTree
+    {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public BinarySearchTree(int rootElement)
+        {
+            root = new Node(rootElement);
+        }
+
+        public void Insert(int x)
+        {
+            Node p = FindParentOfNode(x);
+
+            if (x < p.element)
+                p.left = new Node(x);
+            else
+                p.right = new Node(x);
+        }
+
+        public void Remove(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Node FindParentOfNode(int x)
+        {
+            if (x == root.element)
+                return root;
+            Node p = root;
+            Node i = root;
+            while (i != null)
+            {
+                if (x == i.element)
+                    break;
+                p = i;
+                i = (x < i.element) ? i.left : i.right;
+            }
+            return p;
+        }
+
+        public void RemoveMin()
+        {
+            Node p = root;
+            Node min = root;
+            while (min.left != null)
+            {
+                p = min;
+                min = min.left;
+            }
+            p.left = min.right;
+        }
+
+        public int FindMin()
+        {
+            Node itr = root;
+            while (itr.left != null)
+                itr = itr.left;
+            return itr.element;
+        }
+
+        public string InOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            if (root == null)
+                return "[ ]";
+            return ToString(root, "");
+        }
+
+        private string ToString(Node itr, string str)
+        {
+            if (itr.left == null && itr.right == null)
+                return $"[ NULL {itr.element} NULL ]";
+            else if (itr.left == null)
+                return $"[ NULL {itr.element} {ToString(itr.right, str)} ]";
+            else if (itr.right == null)
+                return $"[ {ToString(itr.left, str)} {itr.element} NULL ]";
+            return $"[ {ToString(itr.left, str)} {itr.element} {ToString(itr.right, str)} ]";
+        }
+
+        private class Node
+        {
+            public int element;
+            public Node left;
+            public Node right;
+
+            public Node(int x)
+            {
+                element = x;
+                left = null;
+                right = null;
+            }
+        }
+    }
+
+
+
+}
--- ADOps/ADOps/Program.cs
+++ ADOps/ADOps/Program.cs
@@ -10,31 +10,45 @@ namespace ADOps
     {
         static void Main(string[] args)
         {
+            #region Les 4
             //// Op2 les 4
+
             //FCNSNode<int> root = new FCNSNode<int>(7);
             //root.Child = new FCNSNode<int>(2, new FCNSNode<int>(1, null, new FCNSNode<int>(5, new FCNSNode<int>(3))), new FCNSNode<int>(9));
             //FCNSTree<int> tree = new FCNSTree<int>(root); 
             //tree.PrintPreOrder();
 
-            // Op3
-            BinaryTree<int> bTree = new BinaryTree<int>(
-                new BinaryNode<int>()
-                {
-                    Data = 4,
-                    Left = new BinaryNode<int>()
-                    {
-                        Data = 2,
-                        Left = new BinaryNode<int>() { Data = 1 },
-                        Right = new BinaryNode<int>() { Data = 3 }
-                    },
-                    Right = new BinaryNode<int>() { Data = 6 }
-                });
+            //// Op3
+            //BinaryTree<int> bTree = new BinaryTree<int>(
+            //    new BinaryNode<int>()
+            //    {
+            //        Data = 4,
+            //        Left = new BinaryNode<int>()
+            //        {
+            //            Data = 2,
+            //            Left = new BinaryNode<int>() { Data = 1 },
+            //            Right = new BinaryNode<int>() { Data = 3 }
+            //        },
+            //        Right = new BinaryNode<int>() { Data = 6 }
+            //    });
 
-            // op4
-            Console.WriteLine(bTree.ToString());
+            //// op4
+            //Console.WriteLine(bTree.ToString());
 
-            Console.WriteLine(bTree.Height());
+            //Console.WriteLine(bTree.Height()); 
+            #endregion
 
+            #region Les 5
+
+            IBinarySearchTree searchTree = new BinarySearchTree(6);
+            searchTree.Insert(4);
+            searchTree.Insert(5);
+            searchTree.Insert(7);
+            searchTree.Insert(2);
+            Console.WriteLine(searchTree);
+            #endregion
+
+            Console.WriteLine("\n\n\nEND");
             Console.ReadLine();
         }
     }
